1967f5dc4cfbaeab03991085091f148a9910acba
Matu36 <matipineda85@live.com.ar>
2023-06-11 15:16:18 -0300

    paginado

--- cucaibafront/src/components/GetAgentes.jsx
+++ cucaibafront/src/components/GetAgentes.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { getAgentes } from "../Redux/Actions";
+import Paginacion from "./Paginacion";
 
 const GetAgentes = () => {
   let dispatch = useDispatch();
@@ -10,8 +11,7 @@ const GetAgentes = () => {
 
   const agentes = useSelector((state) => state.agentes);
 
-
-                        //SEARCHBAR
+  //SEARCHBAR
 
   const [search, setSearch] = useState("");
   const [agente, setAgente] = useState(agentes);
@@ -36,7 +36,29 @@ const GetAgentes = () => {
   useEffect(() => {
     filterByLastName(search);
   }, [agentes, search]);
-                              //FIN SEARCHBAR
+  //FIN SEARCHBAR
+
+  //PAGINADO
+
+  const [currentPage, setCurrentPage] = useState(1); //Pagina Actual seteada en 1
+  const [numberOfPage, setNumberOfPage] = useState(0); //Numero de Paginas seteado en 0
+  const [totalAgentes, setTotalAgentes] = useState(agente);
+
+  const indexFirstPageIngredient = () => (currentPage - 1) * 9; // Indice del primer Elemento
+  const indexLastPageIngredient = () => indexFirstPageIngredient() + 9; //Indice del segundo elemento
+
+  const handlePageNumber = (number) => {
+    setCurrentPage(number);
+  };
+
+  useEffect(() => {
+    setTotalAgentes(
+      agente.slice(indexFirstPageIngredient(), indexLastPageIngredient())
+    );
+    setNumberOfPage(Math.ceil(agente.length / 9)); // cambiando el estado local de numeros de paginas a renderiza
+  }, [agente, currentPage]);
+
+  //FIN PAGINADO
 
   return (
     <div>
@@ -62,7 +84,7 @@ const GetAgentes = () => {
           </tr>
         </thead>
         <tbody>
-          {agente.map((agente) => (
+          {totalAgentes.map((agente) => (
             <tr key={agente.id}>
               <td>{agente.id}</td>
               <td>{agente.apellido}</td>
@@ -73,6 +95,13 @@ const GetAgentes = () => {
           ))}
         </tbody>
       </table>
+      {agentes && (
+        <Paginacion
+          currentPage={currentPage}
+          numberOfPage={numberOfPage}
+          handlePageNumber={handlePageNumber}
+        />
+      )}
     </div>
   );
 };
