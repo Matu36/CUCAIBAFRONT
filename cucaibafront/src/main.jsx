ea5c9011152f827e23737f0ae0555ca37252c392
Agustin <agustinsurila@gmail.com>
2023-06-12 19:58:23 -0300

    Modularización de las rutas

--- cucaibafront/src/main.jsx
+++ cucaibafront/src/main.jsx
@@ -1,54 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "bootstrap/dist/css/bootstrap.min.css";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import { store } from "../src/Redux/Store/index.js";
-import { Provider } from "react-redux";
-import Layout from "./components/Layout/Layout.jsx";
-import GetAgentes from "./components/GetAgentes.jsx";
-import { Home } from "./Pages/Home.jsx";
-import PostAgentes from "./components/PostAgentes.jsx";
-import GetOperativos from "./components/GetOperativos.jsx";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { RouterProvider } from 'react-router-dom';
+import { store } from '../src/Redux/Store/index.js';
+import { Provider } from 'react-redux';
+import router from './utils/routes.jsx';
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Layout />,
-    children: [
-      {
-        path: "/",
-        element: <Home />
-      },
-      {
-        path: "/agentes",
-        children: [
-          {
-            path: "/agentes/ver-agentes",
-            element: <GetAgentes />,
-          },
-          {
-            path: "/agentes/crear-agente",
-            element: <PostAgentes />
-          },
-        ]
-      },
-      {
-        path:"operativos",
-        children: [
-          {
-            path:"/operativos/ver-operativos",
-            element: <GetOperativos />
-          }
-        ]
-      }
-    ]
-  },
-])
-
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <RouterProvider router={router} />
     </Provider>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
