c9881c18f60215c9e0224ca01cf581b4856fe46c
Auchan22 <agustinsurila@gmail.com>
2023-06-13 09:15:37 -0300

    Creacion de pagina de Crear Operativo

---
+++ cucaibafront/src/Pages/Crear-Operativo.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import PostOperativos from '../components/PostOperativos'
+
+export const CrearOperativo = () => {
+  return (
+    <div className='container p-4'>
+      <div>
+        <h1> Crear Operativo</h1>
+        <hr />
+      </div>
+
+      <PostOperativos />
+    </div>
+  )
+}
