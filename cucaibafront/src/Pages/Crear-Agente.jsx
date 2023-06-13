1a8aa20443262fc28970c357f2fafcba420faae0
Auchan22 <agustinsurila@gmail.com>
2023-06-13 08:59:29 -0300

    Creacion de Pagina para Crear Agente

---
+++ cucaibafront/src/Pages/Crear-Agente.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import PostAgentes from '../components/PostAgentes';
+
+const CrearAgente = () => {
+  return (
+    <div className='container p-4'>
+        <div>
+            <h1>Creación de Agente</h1>
+            <hr />
+        </div>
+        <PostAgentes />
+        
+    </div>
+  )
+}
+
+export default CrearAgente;
