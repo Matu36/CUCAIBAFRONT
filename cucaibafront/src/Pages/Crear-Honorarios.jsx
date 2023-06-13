d58e373099d220626b2b0720e0b7fc716ab78e05
Auchan22 <agustinsurila@gmail.com>
2023-06-13 09:46:49 -0300

    Pagina de Creacion de Honorarios

---
+++ cucaibafront/src/Pages/Crear-Honorarios.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import PostHonorarios from "../components/PostHonorarios";
+
+export const CrearHonorarios = () => {
+  return (
+
+    <div>Crear-Honorarios
+
+    <PostHonorarios />
+    </div>
+  )
+}
