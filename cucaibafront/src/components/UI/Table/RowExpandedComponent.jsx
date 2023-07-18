d05ed8f60ec8dd2bf287545b810cbcd407290145
Auchan22 <agustinsurila@gmail.com>
2023-07-18 20:43:31 -0300

    *FEATURE:* Se añadio fetcheo de los modulos pertenencientes al agente en ese operativo

--- cucaibafront/src/components/UI/Table/RowExpandedComponent.jsx
+++ cucaibafront/src/components/UI/Table/RowExpandedComponent.jsx
@@ -3,21 +3,62 @@ import { useHonorarios } from "../../../hooks/useHonorarios";
 import Modal from "../Modal";
 import { FormHonorario } from "../../FormHonorario";
 import { AiOutlinePlus } from "react-icons/ai";
+import { useOperativo } from "../../../hooks/useOperativo";
 
 const RowExpandedComponent = ({ data: operativo }) => {
-  const { data, isLoading } = useHonorarios(operativo.id).honorariosQuery;
+  const { data: agentes, isLoading: loadingAgentes } = useOperativo(
+    operativo.id
+  ).agentesOperativoQuery;
+
   const [modalData, setModalData] = useState({
     operativoID: operativo.id,
-    agenteID: "",
+    agenteID: 0,
   });
+
+  const {
+    data: honorariosAgente,
+    isLoading: honorariosLoading,
+    isError,
+    refetch,
+  } = useHonorarios(operativo.id, modalData.agenteID).honorariosAgenteQuery;
 
   const handleClick = (id) => {
     setModalData({ ...modalData, agenteID: id });
+    refetch();
   };
-
   return (
     <>
-      <Modal title="Hola Mundo" data={modalData} referenceID="formModal">
+      <Modal
+        title="Agregar función al Agente"
+        data={modalData}
+        referenceID="formModal"
+      >
+        <div className="p-3">
+          <h4>Modulos Pendientes del Agente</h4>
+          <hr />
+          <table className="table table-responsive">
+            <thead>
+              <tr>
+                <th scope="col">Descripción</th>
+                <th scope="col">Valor</th>
+              </tr>
+            </thead>
+            <tbody>
+              {typeof honorariosAgente == "object" &&
+                honorariosAgente.map((h) => (
+                  <tr key={h.id}>
+                    <td>{h.modulo.descripcion}</td>
+                    <td>$ {h.valor}</td>
+                  </tr>
+                ))}
+              {honorariosLoading && (
+                <tr>
+                  <td colSpan={2}>Cargando...</td>
+                </tr>
+              )}
+            </tbody>
+          </table>
+        </div>
         <FormHonorario />
       </Modal>
       <div>
@@ -33,22 +74,22 @@ const RowExpandedComponent = ({ data: operativo }) => {
               <table className="table table-responsive">
                 <thead>
                   <tr>
-                    <th scope="col">Apellido</th>
+                    <th scope="col">Descripción</th>
                     <th scope="col">Nombre</th>
                     <th scope="col">CUIL</th>
                     <th scope="col">Acción</th>
                   </tr>
                 </thead>
-                {isLoading && (
+                {loadingAgentes && (
                   <tbody>
                     <tr>
                       <td colSpan={4}>Cargando...</td>
                     </tr>
                   </tbody>
                 )}
-                {!isLoading && typeof data === "object" ? (
+                {!loadingAgentes && typeof agentes === "object" ? (
                   <tbody>
-                    {data.map((agente) => (
+                    {agentes.map((agente) => (
                       <tr key={agente.persona_id}>
                         <td>{agente.apellido}</td>
                         <td>{agente.nombre}</td>
@@ -59,7 +100,9 @@ const RowExpandedComponent = ({ data: operativo }) => {
                             className="btn btn-primary btn-sm"
                             data-bs-toggle="modal"
                             data-bs-target="#formModal"
-                            onClick={() => handleClick(agente.id)}
+                            onClick={() => {
+                              handleClick(agente.id);
+                            }}
                           >
                             <AiOutlinePlus /> Agregar Función
                           </button>
