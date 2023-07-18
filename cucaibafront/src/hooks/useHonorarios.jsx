98b85dee2dc6df7a6f2403ad094901bcf8720fe7
Auchan22 <agustinsurila@gmail.com>
2023-07-18 20:44:08 -0300

    *FIX:* Se arreglo endpoint y se creo nueva query para obtener modulos del agente en x operativo

--- cucaibafront/src/hooks/useHonorarios.jsx
+++ cucaibafront/src/hooks/useHonorarios.jsx
@@ -1,7 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
 import { HonorariosAPI } from "../api/HonorariosAPI";
 
-const getHonorarios = async (operativoId = 0) => {
+const getHonorarios = async (operativoId) => {
   const { data } = await HonorariosAPI.get(
     `/${operativoId != 0 ? `operativo/${operativoId}` : ""}`
   );
@@ -9,13 +9,28 @@ const getHonorarios = async (operativoId = 0) => {
   return data[0];
 };
 
-export const useHonorarios = (operativoId) => {
+const getHonorariosByAgente = async (agenteId, operativoId) => {
+  const { data } = await HonorariosAPI.get(
+    `/modulo/${agenteId}/${operativoId}`
+  );
+
+  return data[0];
+};
+
+export const useHonorarios = (operativoId = 0, agenteId = 0) => {
   const honorariosQuery = useQuery({
     queryKey: ["honorarios", { operativoId }],
     queryFn: () => getHonorarios(operativoId),
   });
 
+  const honorariosAgenteQuery = useQuery({
+    queryKey: ["honorarios-agente", { agenteId, operativoId }],
+    queryFn: () => getHonorariosByAgente(agenteId, operativoId),
+    enabled: agenteId != 0,
+  });
+
   return {
     honorariosQuery,
+    honorariosAgenteQuery,
   };
 };
