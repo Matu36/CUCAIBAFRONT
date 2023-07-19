05c2b90292443f354aeef2095f4ebcfb2482ea83
Auchan22 <agustinsurila@gmail.com>
2023-07-19 14:20:35 -0300

    *FIX:* Corrigiendo estilos de header de React Data Table

--- cucaibafront/src/main.jsx
+++ cucaibafront/src/main.jsx
@@ -5,13 +5,15 @@ import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import { Provider } from "react-redux";
 
-import {QueryClient, QueryClientProvider} from "@tanstack/react-query"
-import {ReactQueryDevtools} from "@tanstack/react-query-devtools"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 import { store } from "./Redux/Store/index";
 import router from "./utils/routes";
 
 const queryClient = new QueryClient();
+
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
@@ -21,5 +23,5 @@ ReactDOM.createRoot(document.getElementById("root")).render(
         <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
     </Provider>
-  </React.StrictMode>,
+  </React.StrictMode>
 );
