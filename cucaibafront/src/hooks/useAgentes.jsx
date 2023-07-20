ca0f5e5fc3e29e69b4063993913d54392d09e2dd
Auchan22 <agustinsurila@gmail.com>
2023-07-20 14:41:59 -0300

    *FIX:* Se arreglo parseo de data

--- cucaibafront/src/hooks/useAgentes.jsx
+++ cucaibafront/src/hooks/useAgentes.jsx
@@ -1,46 +1,46 @@
-import { useQuery } from '@tanstack/react-query';
-import { AgentesAPI } from '../api/AgentesAPI';
-import { useEffect, useState } from 'react';
+import { useQuery } from "@tanstack/react-query";
+import { AgentesAPI } from "../api/AgentesAPI";
+import { useEffect, useState } from "react";
 
-const getAgentes = async ()=> {
+const getAgentes = async () => {
   // console.log(args);
 
-//   const params = new URLSearchParams();
+  //   const params = new URLSearchParams();
 
-//   if (state) params.append('state', state);
+  //   if (state) params.append('state', state);
 
-//   if (labels.length > 0) {
-//     const labelString: string = labels.join(',');
-//     params.append('labels', labelString);
-//   }
+  //   if (labels.length > 0) {
+  //     const labelString: string = labels.join(',');
+  //     params.append('labels', labelString);
+  //   }
 
-//   params.append('page', page?.toString());
-//   params.append('per_page', '5');
+  //   params.append('page', page?.toString());
+  //   params.append('per_page', '5');
 
-  const { data } = await AgentesAPI.get('/');
+  const { data } = await AgentesAPI.get("/");
 
-  return data;
+  return data[0];
 };
 
 export const useAgentes = () => {
-//   const [page, setPage] = useState<number>(1);
+  //   const [page, setPage] = useState<number>(1);
 
-//   useEffect(() => {
-//     setPage(1);
-//   }, [state, labels]);
+  //   useEffect(() => {
+  //     setPage(1);
+  //   }, [state, labels]);
 
-//   const nextPage = () => {
-//     if (issuesQuery.data?.length === 0) return;
-//     setPage((p) => p + 1);
-//   };
+  //   const nextPage = () => {
+  //     if (issuesQuery.data?.length === 0) return;
+  //     setPage((p) => p + 1);
+  //   };
 
-//   const prevPage = () => {
-//     if (page > 1) setPage((p) => p - 1);
-//   };
+  //   const prevPage = () => {
+  //     if (page > 1) setPage((p) => p - 1);
+  //   };
 
   // Por medio de los {}, podemos definir keys, pero sin importar el orden, ya que va a saber a cual elemento apunta
   const agentesQuery = useQuery({
-    queryKey: ['agentes'],
+    queryKey: ["agentes"],
     queryFn: () => getAgentes(),
   });
 
