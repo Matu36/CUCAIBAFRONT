d93aab91c7be1d5a293249c72595f65cf7413c1c
Auchan22 <agustinsurila@gmail.com>
2023-07-21 12:27:39 -0300

    *FEATURE:* Creación de hook para traer los datos de las personas

---
+++ cucaibafront/src/hooks/usePersona.jsx
@@ -0,0 +1,20 @@
+import { useQuery } from "@tanstack/react-query";
+import { PersonasAPI } from "../api/PersonasAPI";
+
+const getPersonaByDNI = async (dni) => {
+  const { data } = await PersonasAPI.get(`/${dni}`);
+
+  return data[0];
+};
+
+export const usePersona = (dni = 0) => {
+  const personaQuery = useQuery({
+    queryKey: ["persona", dni],
+    queryFn: () => getPersonaByDNI(dni),
+    enabled: dni != 0,
+  });
+
+  return {
+    personaQuery,
+  };
+};
