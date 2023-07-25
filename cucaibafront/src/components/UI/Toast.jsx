215824faf55985d5357b74546207c48caaf12185
Agustin <agustinsurila@gmail.com>
2023-07-25 19:19:49 -0300

    *FIX:* Mejorando la logica del Toast

--- cucaibafront/src/components/UI/Toast.jsx
+++ cucaibafront/src/components/UI/Toast.jsx
@@ -1,36 +1,30 @@
 import React from "react";
-
-let toastEl = document.getElementById("liveToast");
-
-export const handleToast = () => {
-  let toast = new bootstrap.Toast(toastEl, {
-    animation: true,
-    autohide: true,
-    delay: 2500,
-  });
-  toast.show();
-};
+import useToast from "../../hooks/useToast";
 
 const Toast = ({ msg }) => {
+  const { setShow, show } = useToast();
+
   return (
     <div className="position-fixed bottom-0 end-0 p-3">
       <div
         id="liveToast"
-        className="toast d-flex align-items-center justify-content-between px-3 text-bg-primary"
+        className={`toast ${
+          show ? "showToast" : "hideToast"
+        } d-flex align-items-center justify-content-between px-3 text-bg-primary`}
         role="alert"
         aria-live="assertive"
         aria-atomic="true"
-        style={{ zIndex: 1000 }}
       >
         <div className="toast-body">
           <strong className="me-auto">{msg}</strong>
-          <button
-            type="button"
-            className="btn-close"
-            data-bs-dismiss="toast"
-            aria-label="Close"
-          ></button>
         </div>
+        <button
+          type="button"
+          className="btn-close"
+          data-bs-dismiss="toast"
+          aria-label="Close"
+          onClick={() => setShow(false)}
+        ></button>
       </div>
     </div>
   );
