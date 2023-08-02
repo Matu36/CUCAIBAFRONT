68a2ce8c7a97f5f4f549744aa1f29483c0664dca
Matu36 <you@example.com>
2023-08-02 12:13:13 -0300

    ordendepagoestilo

--- cucaibafront/src/components/OrdenDetail.jsx
+++ cucaibafront/src/components/OrdenDetail.jsx
@@ -2,18 +2,19 @@ import React from 'react';
 import "../assets/styles/detalle.css";
 import { useOrdenPorLiquidacionId } from '../hooks/useOrdenesDePago';
 import Spinner from "../components/UI/Spinner";
+import "../assets/styles/detalle.css"
 
 export const OrdenDetail = () => {
   const liquidacion_id = 41;
   const { data, isFetched } = useOrdenPorLiquidacionId(liquidacion_id).ordenesPorIdQuery;
 
-  // Verificar si data es un array y no un objeto
-  const personasArray = Array.isArray(data) ? data[0] : []; // Si data es undefined o null, personasArray será un array vacío
+ 
+  const personasArray = Array.isArray(data) ? data[0] : []; 
 
   return (
     <div className='card'>
       {isFetched ? (
-        <table>
+        <table className="styled-table">
           <thead>
             <tr>
               <th>Nombre</th>
@@ -34,12 +35,12 @@ export const OrdenDetail = () => {
                     <td>
                       {detalles.items.map((item, itemIndex) => (
                         <div key={itemIndex}>
-                          <span>Descripción: {item.descripcion}</span>
-                          <span>Valor Unitario: {item.valor_unitario}</span>
+                          <span>Función: {item.descripcion}</span>
+                          <span>Valor Unitario: $ {item.valor_unitario}</span>
                         </div>
                       ))}
                     </td>
-                    <td>{detalles.valor_total}</td>
+                    <td>$ {detalles.valor_total}</td>
                   </tr>
                 );
               });
