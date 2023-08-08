5efc4fedd58c9062dbd558e63fceeac100b52da0
Matu36 <you@example.com>
2023-08-08 11:23:17 -0300

    PDF

---
+++ cucaibafront/src/components/PDFordenDePago.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {
+    PDFDownloadLink,
+    Document,
+    Page,
+    View,
+    Text,
+    StyleSheet,
+  } from "@react-pdf/renderer";
+
+export const PDFordenDePago = () => {
+  return (
+    <div>PDFordenDePago</div>
+  )
+}
