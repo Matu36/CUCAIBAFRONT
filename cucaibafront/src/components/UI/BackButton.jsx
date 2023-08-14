767d147b1317d615b5322e7cc6c4564892ba2ca8
Auchan22 <agustinsurila@gmail.com>
2023-08-14 09:10:27 -0300

    *FIX:* Arreglando Boton Volver

--- cucaibafront/src/components/UI/BackButton.jsx
+++ cucaibafront/src/components/UI/BackButton.jsx
@@ -1,15 +1,14 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import { ImArrowLeft } from "react-icons/im";
 
 const BackButton = () => {
   const navigate = useNavigate();
   return (
-    <button onClick={() => navigate(-1)} className="btn outline-primary">
-      <i className="bi bi-arrow-left">
-        {" "}
-        <ImArrowLeft />{" "}
-      </i>
+    <button
+      onClick={() => navigate(-1)}
+      className="btn btn-secondary outline-primary"
+    >
+      Volver
     </button>
   );
 };
