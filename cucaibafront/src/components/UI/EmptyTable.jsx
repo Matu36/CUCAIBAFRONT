293272f7ab5f6a6f12959ddef1c1d2e0c323bfe8
Auchan22 <agustinsurila@gmail.com>
2023-08-30 14:52:06 -0300

    *FIX:* Permitiendo children

--- cucaibafront/src/components/UI/EmptyTable.jsx
+++ cucaibafront/src/components/UI/EmptyTable.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from "react";
 
 // Usado en el Datatable para mostrar el mensaje vacio
 
-const EmptyTable = ({msg}) => {
+const EmptyTable = ({ msg, children }) => {
   return (
-    <div className='p-3 fw-bold'>{msg}</div>
-  )
-}
+    <div className="d-flex align-items-center justify-content-center flex-column">
+      <div className="p-3 fw-bold">{msg}</div>
+      {children}
+    </div>
+  );
+};
 
-export default EmptyTable
+export default EmptyTable;
