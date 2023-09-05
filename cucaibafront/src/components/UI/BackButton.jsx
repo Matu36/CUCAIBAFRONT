25e8dbf7540ff1eebef729d7c1902c925dff6597
Auchan22 <agustinsurila@gmail.com>
2023-09-05 19:49:05 -0300

    Arreglando back button

--- cucaibafront/src/components/UI/BackButton.jsx
+++ cucaibafront/src/components/UI/BackButton.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import { FaAngleLeft } from "react-icons/fa";
 
 // Boton de volver a la página anterior
 
 const BackButton = () => {
   const navigate = useNavigate();
   return (
-    <button onClick={() => navigate(-1)} className="btn btn-back">
-      Volver
+    <button onClick={() => navigate(-1)} className="btn btn-volver-super">
+      <FaAngleLeft className="mr-2" size="1.25em" /> Volver
     </button>
   );
 };
