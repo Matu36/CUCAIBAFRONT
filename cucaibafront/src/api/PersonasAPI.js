e0957a8e34fc0bcaf3d89bc6bd84738c7ce730f1
Matu36 <matipineda85@live.com.ar>
2023-10-04 10:15:46 -0300

    endpointPersonas

--- cucaibafront/src/api/PersonasAPI.js
+++ cucaibafront/src/api/PersonasAPI.js
@@ -4,8 +4,7 @@ import axios from "axios";
 //establecer la seguridad con symfony y los metodos GET, PUT Y POST para evitar problemas de CORS. (Se configura en symfony el paquete NELMIOS)
 
 export const PersonasAPI = axios.create({
-  baseURL: `${import.meta.env.VITE_BACKEND_URL}/personas`,
-  withCredentials: true,
+  baseURL: `${import.meta.env.VITE_BACKEND_URL_PERSONAS}/personas`,
   mode: "cors",
   headers: {
     "Access-Control-Allow-Origin": "*",
